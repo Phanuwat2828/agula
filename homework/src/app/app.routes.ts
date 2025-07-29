abb24bceef474f7f5babc9829ed8ff71e8143c40
Phanuwat Khamtha <131578554+Phanuwat2828@users.noreply.github.com>
2025-07-29 17:13:17 +0700

    update_all

--- homework/src/app/app.routes.ts
+++ homework/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Movie } from './pages/movie/movie';
 import { Main } from './pages/main/main';
+import { Detail } from './pages/detail/detail';
+import { Pagenotfound } from './pages/pagenotfound/pagenotfound';
 export const routes: Routes = [
     {path:"",component:Main},
     {path:"movies",component:Movie},
+    {path:"detail",component:Detail},
+    {path:"**",component:Pagenotfound}
 ];
--- homework/src/app/pages/movie/movie.ts
+++ homework/src/app/pages/movie/movie.ts
@@ -1,9 +1,8 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { Nav } from '../../components/nav/nav';
 import { CommonModule } from '@angular/common';
-// import { DataService } from '../../service/datajson/datajson';
 import myData from '../../../assets/data.json';
-
+import { RouterModule,Router,RouterLink,RouterOutlet } from '@angular/router';
 
 interface Movie_type {
   name: string;
@@ -27,7 +26,7 @@ interface Creator {
 })
 
 export class Movie implements AfterViewInit {
-  
+  constructor(private router: Router){}
   currentIndex = 0;
   autoScrollInterval: any;
   movies: NodeListOf<Element> | undefined;
@@ -35,8 +34,12 @@ export class Movie implements AfterViewInit {
   directorSlider: HTMLElement | null = null;
   jsonData: { [key: string]: Movie_type }= {};
   ngOnInit() {
-    this.jsonData = ;
-
+    this.jsonData = myData;
+  }
+  redirect(id:number){
+    this.router.navigate(['/detail'], {
+      queryParams: { id: id }
+    });
   }
   ngAfterViewInit() {
     this.movieSlider = document.getElementById('movieSlider');
@@ -67,20 +70,26 @@ export class Movie implements AfterViewInit {
     }
   }
 
-  updateDirectorInfo() {
+  updateDirectorInfo(index: number) {
+    const creatorList = this.jsonData["movie" + index.toString()]["creator"];
     if (this.directorSlider) {
-      this.directorSlider.innerHTML = '';
-      const activeMovie = document.querySelector('.movie.active');
-      const directorName = activeMovie?.getAttribute('data-director') || '';
+      this.directorSlider.innerHTML = ''; // ล้างก่อน
+      creatorList.forEach(element => {
+        const directorName = element['name'];
 
-      const directorCard = document.createElement('div');
-      directorCard.className = 'director-card';
-      directorCard.innerHTML = `
-        <h4 class="director-name">${directorName}</h4>
-        <p class="director-role">Director</p>
-      `;
+        const directorCard = document.createElement('div');
+        directorCard.className = 'director-card';
+        directorCard.innerHTML = `
+         
+          <div class="creator_container">
+            <img src="${element['image']}" alt="" style="border-radius: 50%;">
+             <h4 style=" color: white;  text-align: center;">${directorName}</h4>
+        </div>
+        `;
 
-      this.directorSlider.appendChild(directorCard);
+        this.directorSlider!.appendChild(directorCard); // ✅ ปลอดภัยเพราะเราเช็กแล้ว
+      });
+
       this.directorSlider.classList.add('show');
     }
   }
@@ -103,7 +112,7 @@ export class Movie implements AfterViewInit {
         behavior: 'smooth'
       });
 
-      this.updateDirectorInfo();
+      this.updateDirectorInfo(index+1);
     }
   }
 }
@@ -112,6 +121,7 @@ export class Movie implements AfterViewInit {
     this.autoScrollInterval = setInterval(() => {
       this.currentIndex = (this.currentIndex + 1) % (this.movies?.length || 0);
       this.updateActiveMovie(this.currentIndex);
+
     }, 3000); 
   }
 
